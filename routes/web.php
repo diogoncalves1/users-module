8fbc7c265db3d1bd1196853de7282c43e2ef927d
diogogoncalves <dmgdark95@gmail.com>
2025-07-24 13:44:54 +0100

    Routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(
+    [
+        "prefix" => "admin",
+        "as" => "admin.",
+        // "middleware" => "auth"
+    ],
+    function () {
+        // add Controller 
+        // add roles manager route
+        Route::group([
+            'prefix' => 'users/manage',
+            'as' => 'users.manage'
+        ], function () {
+            Route::get('/{id}', [\App\Http\Controllers\UserController::class, 'showManageForm']);
+            Route::post('/{id}', [\App\Http\Controllers\UserController::class, 'manage'])->name('.update');
+        });
+        Route::get('users/data', [\App\Http\Controllers\UserController::class, 'dataTable']);
+        Route::resource('users', \App\Http\Controllers\UserController::class, ['except' => 'show']);
+    }
+);
