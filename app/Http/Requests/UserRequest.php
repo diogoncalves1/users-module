60d35244dde5356f8122618293405f8641976c71
diogogoncalves <dmgdark95@gmail.com>
2025-07-24 15:21:11 +0100

    Update Requests

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $rules = [
+            'name' => 'required|string|max:255',
+        ];
+
+        if ($this->get('user_id')) {
+            $rules['email'] = ['required', 'email', Rule::unique('users')->ignore($this->get('user_id')), 'max:191'];
+            $rules['password'] = 'nullable|min:8|max:191';
+        } else {
+            $rules['email'] = 'required|email|unique:users|max:191';
+            $rules['password'] = 'required|min:8|max:191';
+        }
+
+        return $rules;
+    }
+}
