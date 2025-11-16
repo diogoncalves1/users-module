274513e0de811d54f68d81fb57cf3320bae379ed
diogogoncalves <dmgdark95@gmail.com>
2025-11-16 17:41:25 +0000

    Update Api Respondet to Version 1.0.1

--- Modules/ApiResponder/Providers/RouteServiceProvider.php
+++ Modules/ApiResponder/Providers/RouteServiceProvider.php
@@ -22,29 +22,29 @@ class RouteServiceProvider extends ServiceProvider
     /**
      * Define the routes for the application.
      */
-    public function map(): void
-    {
-        $this->mapApiRoutes();
-        $this->mapWebRoutes();
-    }
+    // public function map(): void
+    // {
+    //     $this->mapApiRoutes();
+    //     $this->mapWebRoutes();
+    // }
 
     /**
      * Define the "web" routes for the application.
      *
      * These routes all receive session state, CSRF protection, etc.
      */
-    protected function mapWebRoutes(): void
-    {
-        Route::middleware('web')->group(module_path($this->name, '/routes/web.php'));
-    }
+    // protected function mapWebRoutes(): void
+    // {
+    //     Route::middleware('web')->group(module_path($this->name, '/routes/web.php'));
+    // }
 
     /**
      * Define the "api" routes for the application.
      *
      * These routes are typically stateless.
      */
-    protected function mapApiRoutes(): void
-    {
-        Route::middleware('api')->prefix('api')->name('api.')->group(module_path($this->name, '/routes/api.php'));
-    }
+    // protected function mapApiRoutes(): void
+    // {
+    //     Route::middleware('api')->prefix('api')->name('api.')->group(module_path($this->name, '/routes/api.php'));
+    // }
 }
